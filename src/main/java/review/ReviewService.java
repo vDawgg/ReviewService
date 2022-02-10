193d13148ad4bfe4cf0135f4cfa567d1d317eed4
vDawgg <veit.laule@gmail.com>
2022-02-10 17:30:29 +0000

    Review Service Server now mostly done; added grpc healthmanager to gradle.

--- src/main/java/review/ReviewService.java
+++ src/main/java/review/ReviewService.java
@@ -2,6 +2,11 @@ package review;
 
 import com.mongodb.MongoException;
 import com.mongodb.client.*;
+import io.grpc.Internal;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.health.v1.HealthCheckResponse;
+import io.grpc.protobuf.services.HealthStatusManager;
 import io.grpc.stub.StreamObserver;
 import org.bson.BsonDocument;
 import org.bson.BsonInt64;
@@ -10,18 +15,75 @@ import org.bson.conversions.Bson;
 import review.service.ReviewServiceGrpc;
 import review.service.ReviewServiceProto;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.logging.Logger;
 
+//TODO: Start testing as much as possible
 public class ReviewService {
 
+    //TODO: decide on tracing with jaeger (java library deprecated) or opentelemetry (version suggested by jaeger)
+    //TODO: Improve logging (for java/gradle and mongodb!)
     private static final Logger logger = Logger.getLogger("ReviewServiceLogger");
 
-    //TODO: decide on tracing with jaeger (java library deprecated) or opentelemetry (version suggested by jaeger)
+    private Server server;
+    private HealthStatusManager healthMgr;
 
     //Objects for mongodb
     private static final MongoClient client = MongoClients.create();
     private static final MongoDatabase db = client.getDatabase("reviewDB");
+
+    //TODO: Create mock-client for interaction with the server
+
+    /**
+     * Method for starting the grpc server (Mostly taken from implementation of Adservice)
+     * @throws IOException if JVM has shut down
+     */
+    private void start() throws IOException {
+        int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "6666")); //does this part need to be that complicated?
+        healthMgr = new HealthStatusManager();
+
+        server  =
+                ServerBuilder.forPort(port)
+                        .addService(new ReviewServiceImpl())
+                        .addService(healthMgr.getHealthService())
+                        .build()
+                        .start();
+        logger.info("Review Service started, listening on "+port);
+        Runtime.getRuntime()
+                .addShutdownHook(
+                        new Thread(
+                                () -> {
+                                    System.err.println("*** shutting down grpc reviews server since JVM is shutting down");
+                                    ReviewService.this.stop();
+                                    System.err.println("*** server shut down");
+                                }));
+        healthMgr.setStatus("Review", HealthCheckResponse.ServingStatus.SERVING);
+
+        try {
+            Bson command = new BsonDocument("ping", new BsonInt64(1));
+            Document commandResult = db.runCommand(command);
+            logger.info("Connected successfully to server.");
+        } catch (MongoException me) {
+            logger.info("An error occurred while attempting to run a command: " + me);
+            return;
+        }
+
+        if(!db.listCollectionNames().into(new ArrayList<>()).contains("reviews")) {
+            db.createCollection("reviews");
+        }
+    }
+
+    /**
+     * Stops the server and tells other services through the health manager
+     * Copied from the AdService implementation
+     */
+    private void stop() {
+        if(server!=null) {
+            healthMgr.clearStatus("Review"); //This might have to applied to the ReviewService explicitly
+            server.shutdown();
+        }
+    }
 
     private static class ReviewServiceImpl extends ReviewServiceGrpc.ReviewServiceImplBase {
 
@@ -73,20 +135,28 @@ public class ReviewService {
         }
     }
 
-    public static void main(String[] args) {
-
-        //Testing of connection below - Taken from https://docs.mongodb.com/drivers/java/sync/current/fundamentals/connection/connect/
-        try {
-            Bson command = new BsonDocument("ping", new BsonInt64(1));
-            Document commandResult = db.runCommand(command);
-            logger.info("Connected successfully to server.");
-        } catch (MongoException me) {
-            logger.info("An error occurred while attempting to run a command: " + me);
-            return;
+    /**
+     * Taken from AdService
+     * @throws InterruptedException
+     */
+    private void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
         }
+    }
+
+    public static void main(String[] args) {
 
         if(!db.listCollectionNames().into(new ArrayList<>()).contains("reviews")) {
             db.createCollection("reviews");
+        }
+
+        final ReviewService service = new ReviewService();
+        try {
+            service.start();
+            service.blockUntilShutdown();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
         /**Should be working fine now, but still here to fall back on
